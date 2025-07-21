09833e2f3dec6850061d4c2c07a99685f076d998
Seonghoon Jeong <hooni09061201@gmail.com>
2025-07-21 10:14:55 +0900

    apply_font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './src/**/*.{vue,js,jsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Jal_Haru', 'sans-serif'], // Tailwind 기본 sans를 덮어씀
+         pretendard: ['Pretendard-Regular', 'sans-serif'], // 나중에 사용할 때 class = "font-pretendard" 적용하면 됨
+      },
+    },
   },
   plugins: [],
-}
+};
