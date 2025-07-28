a0b88b643dd0c485948e57f6b3f105bd1a9fb09d
yeomjungwoo <yeomi0206@gmail.com>
2025-07-28 11:33:26 +0900

    fix: fix scroll issue #131

--- src/router/index.js
+++ src/router/index.js
@@ -79,6 +79,9 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  }
 })
 
 export default router
