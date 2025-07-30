bfdf72b182859de2d4daf1772e5b50fd26d44f96
Seonghoon Jeong <hooni09061201@gmail.com>
2025-07-30 09:56:21 +0900

    refactor_login_api

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,32 @@
-// 로그인 api
+// src/api/Authapi.js
+import api from './axios'
 
-import api from '@/api/axios'
-const BASE_URL = '/auth/'
-
-export default {}
+export default {
+    login({ user_id, password }) {
+        return api.post('/auth/login', { user_id, password })
+    },
+    signup({ user_id, password, name, phone, birthdate }) {
+        return api.post('/auth/register', {
+            user_id,
+            password,
+            name,
+            phone,
+            birthdate,
+        })
+    },
+    requestResetPassword(email) {
+        return api.post('/auth/find-password', { email })
+    },
+    verifyResetCode({ email, code }) {
+        return api.post('/auth/verify-code', { email, code })
+    },
+    resetPassword({ email, newPassword }) {
+        return api.post('/auth/reset-password', { email, newPassword })
+    },
+    refreshToken(refresh_token) {
+        return api.post('/auth/refresh', { refresh_token })
+    },
+    logout() {
+        return api.post('/auth/logout')
+    },
+}
